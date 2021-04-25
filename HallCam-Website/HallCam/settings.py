4a6381a2aa627dceeaef5956f3c294e311d97d73
Carsten Fuchs <carsten.fuchs@cafu.de>
2021-04-25 15:11:29 +0200

    Use a custom user model right from the start
    See https://django-improved-user.readthedocs.io for details.

--- HallCam-Website/HallCam/settings.py
+++ HallCam-Website/HallCam/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -81,12 +82,17 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'Accounts.User'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {
+            'user_attributes': ('email', 'full_name', 'short_name'),
+        },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
