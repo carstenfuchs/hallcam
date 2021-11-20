116b323aefe6efbb53f43404c164aec1770f4cba
Carsten Fuchs <carsten.fuchs@cafu.de>
2021-11-20 13:20:37 +0100

    Add basic support for uploading pictures

--- rpi/run.py
+++ rpi/run.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import os
+import requests
 from time import sleep
 from picamera import PiCamera
+
+import localconfig
 
 
 PICTURES_DIR = '/var/HallCam/pictures/'
@@ -10,6 +13,9 @@ if not os.access(PICTURES_DIR, os.W_OK):
     print(f"Cannot access {PICTURES_DIR}.")
 else:
     print(f"Access to {PICTURES_DIR} is good.")
+
+print(f"localconfig.UPLOAD_URL = {localconfig.UPLOAD_URL}")
+print("")
 
 
 camera = PiCamera()
@@ -34,7 +40,27 @@ camera.start_preview()
 for i in range(5):
     sleep(5)
     filename = f'{PICTURES_DIR}/image_{i:03d}.jpg'
+
     camera.capture(filename)
     print(f"Captured {filename}")
+
+    if localconfig.UPLOAD_URL:
+        with open(filename, 'rb') as pic_file:
+            try:
+                r = requests.post(
+                    localconfig.UPLOAD_URL,
+                    files={'pic_file': pic_file},
+                    allow_redirects=False,
+                    timeout=10.0,
+                )
+                print(f"Uploaded picture to {localconfig.UPLOAD_URL}")
+                if r.status_code != 302:
+                    print(f"Unexpected response: Expected status_code 302, got status_code {r.status_code}.")
+                    print(r)
+                    print(r.text)
+            except requests.exceptions.Timeout as e:
+                print(f"Requests raised a timeout exception: {e}")
+            except requests.exceptions.RequestException as e:
+                print(f"Requests raised an exception: {e}")
 
 camera.stop_preview()
