deb8fb70b32aa0038a43a0cbb7c987f57702c9aa
Carsten Fuchs <carsten.fuchs@cafu.de>
2021-11-25 09:39:05 +0100

    web: Delete Picture objects whose image file cannot be found
    If we removed an image file from the storage on disk, accessing the Picture
    object that refers to it raises an exception. Thus delete Picture objects
    that refer to files that cannot be found.

--- web/Core/views/viewer.py
+++ web/Core/views/viewer.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from pathlib import Path
 
 from Core.models import Picture
 
 
 def viewer_view(request):
     all_pics = Picture.objects.all()
+    missing_paths = []
+
+    for pic in all_pics:
+        p = pic.picture.path
+        if not Path(p).is_file():
+            missing_paths.append(p)
+            pic.delete()
+
+    if missing_paths:
+        all_pics = Picture.objects.all()
 
     return render(request, 'Core/viewer.html', {
         'pics': all_pics,
+        'missing_paths': missing_paths,
     })
