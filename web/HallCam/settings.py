ecbee558af3ea59ea317aa252e9bce49bbf7b09f
Carsten Fuchs <carsten.fuchs@cafu.de>
2021-11-25 10:22:16 +0100

    web: Introduce local settings for the HallCam project

--- web/HallCam/settings.py
+++ web/HallCam/settings.py
@@ -11,21 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from HallCam import localconfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = localconfig.DEBUG
+SECRET_KEY = localconfig.SECRET_KEY
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#7!4&m=-eh*491fjcr_%r9cvc7lewmiem1m&z@@77=(%ku2c%k'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = localconfig.ALLOWED_EXTRA_HOSTS + [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
