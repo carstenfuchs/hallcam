e9d65e512360f714bb2f41ef1ad3e1783f7c9d48
Carsten Fuchs <carsten.fuchs@cafu.de>
2021-12-05 11:22:10 +0100

    rpi: Rename `Scheduler` to `Task` and add new task `TaskCleanupDisk`

---
+++ rpi/common.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+
+def fmt_bytes(size, decimal_places=2):
+    for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']:
+        if size < 1024.0 or unit == 'PiB':
+            break
+        size /= 1024.0
+
+    return f"{size:.{decimal_places}f} {unit}"
+
+
+def get_pic_stem_from_data(dt, importance):
+    return f'pic_{dt.strftime("%Y%m%d_%H%M%S")}_{importance}'
+
+
+def get_data_from_pic_stem(pic_stem):
+    try:
+        dt = datetime.strptime(pic_stem[4:19], "%Y%m%d_%H%M%S")
+    except ValueError:
+        dt = datetime(2000, 1, 1)
+
+    try:
+        importance = int(pic_stem[20:21])
+    except ValueError:
+        importance = 0
+
+    cross_check = get_pic_stem_from_data(dt, importance)
+    if pic_stem != cross_check:
+        # print(f"Bad pic_stem: {pic_stem} != {cross_check}")
+        pass
+
+    return dt, importance
+
+
+def get_score(dt, importance, now):
+    """
+    This function computes the score that indicates the significance of a picture.
+    The significance determines the order in which pictures are deleted when disk
+    space gets scarce.
+
+    Some notes:
+
+      - Older pictures should be deleted before newer pictures.
+      - Less important pictures should be deleted before more important pictures.
+
+    For example, a picture that is unimportant but only a day old could have
+    the same significance (priority for deletion) as another picture that is
+    very important but now several years old.
+
+    That is, if we put the age and the importance of a picture into a coordinate
+    system, the picture with the coordinate p1 = (age: 1 day, importance: 1)
+    should have the same score as a picture at coordinate p2 = (age: 8 years,
+    importance: 8).
+
+    Note that we can interpolate between p1 and p2 and that the line through
+    p1 and p2 can be considered a 2-dimensional hyperplane. The distance of any
+    point to that hyperplane can be taken as the score of that point.
+    """
+    age = (now - dt).total_seconds() / 3600.0   # age in hours
+    vec = (age, importance)
+    nrm = (-(8 - 1), (365*8 - 1)*24.0)
+
+    return vec[0]*nrm[0] + vec[1]*nrm[1]
--- rpi/run.py
+++ rpi/run.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 import os
-import requests
-from datetime import datetime
+import requests, shutil
+from datetime import datetime, timedelta
 from time import sleep
+from pathlib import Path
 from picamera import PiCamera
 
 import localconfig
+from common import fmt_bytes, get_pic_stem_from_data, get_data_from_pic_stem, get_score
 
 
-class Scheduler:
+class Task:
     def is_due(self, dt0, dt1):
         """
-        Returns the importance with which the Scheduler thinks that a picture
-        should be taken for some time t in intervall dt0 < t <= dt1.
+        Returns the importance with which the Task thinks that a picture
+        should be taken in the intervall dt0 < t <= dt1.
         """
         return 0
 
 
-class SchedulerEvery5Minutes(Scheduler):
+class TaskEvery5Minutes(Task):
     def is_due(self, dt0, dt1):
         ts0 = dt0.timestamp()
         ts1 = dt1.timestamp()
@@ -28,13 +30,83 @@ class SchedulerEvery5Minutes(Scheduler):
         return 0
 
 
-class SchedulerEveryFullHour(Scheduler):
+class TaskEveryFullHour(Task):
     def is_due(self, dt0, dt1):
         ts0 = dt0.timestamp()
         ts1 = dt1.timestamp()
 
         if (ts0 // 3600) < (ts1 // 3600):
             return 6
+
+        return 0
+
+
+def gather_pictures(clean_dir, now):
+    pics = []
+
+    for pic_path in Path(clean_dir).iterdir():
+        pics.append(
+            (
+                pic_path,
+                pic_path.stat().st_size,
+                get_score(*get_data_from_pic_stem(pic_path.stem), now),
+            )
+        )
+
+    # Sort the pictures by score.
+    pics.sort(key=lambda pic: -pic[2])
+    return pics
+
+
+class TaskCleanupDisk(Task):
+    def __init__(self, clean_dir):
+        self.clean_dir = clean_dir
+        self.next_clean_dt = datetime(2000, 1, 1)
+
+    def clean(self, now):
+        print("Cleaning up disk space ...")
+        GiB = pow(1024, 3)
+        MIN_PICS_TOTAL =  1*GiB
+        MAX_PICS_TOTAL = 20*GiB
+        MIN_DISK_FREE  =  2*GiB
+
+        disk_total, disk_used, disk_free = shutil.disk_usage("/")
+        # print(f"disk_total {fmt_bytes(disk_total):>10}")
+        # print(f"disk_used  {fmt_bytes(disk_used):>10}")
+        # print(f"disk_free  {fmt_bytes(disk_free):>10} ({fmt_bytes(MIN_DISK_FREE)})")
+        btd_disk = max(MIN_DISK_FREE - disk_free, 0)
+        print(f"free disk space: {fmt_bytes(disk_free)}, want at least: {fmt_bytes(MIN_DISK_FREE)} --> delete {fmt_bytes(btd_disk)}")
+
+        pics = gather_pictures(self.clean_dir, now)
+        pics_total_bytes = sum(pic[1] for pic in pics)
+        btd_pics = max(pics_total_bytes - MAX_PICS_TOTAL, 0)
+        print(f"{len(pics)} pictures of size: {fmt_bytes(pics_total_bytes)}, want at most: {fmt_bytes(MAX_PICS_TOTAL)} --> delete {fmt_bytes(btd_pics)}")
+
+        pic_bytes_to_delete = max(btd_disk, btd_pics)
+        print(f"--> deleting {fmt_bytes(pic_bytes_to_delete)} ...")
+
+        while pics and pic_bytes_to_delete > 0 and pics_total_bytes > MIN_PICS_TOTAL:
+            pic = pics.pop()
+            print(f"  deleting {pic[0]},{pic[1]:>8} bytes, score {pic[2]}")
+            pic[0].unlink()
+          # pic[0].unlink(missing_ok=True)  # Python 3.8+
+            pic_bytes_to_delete -= pic[1]
+            pics_total_bytes -= pic[1]
+
+        print(f"{len(pics)} pictures of size: {fmt_bytes(pics_total_bytes)} left")
+        if pic_bytes_to_delete > 0:
+            print(f"{fmt_bytes(pic_bytes_to_delete)} not deleted, MIN_PICS_TOTAL is {fmt_bytes(MIN_PICS_TOTAL)}")
+
+    def is_due(self, dt0, dt1):
+        if dt0 < self.next_clean_dt <= dt1:
+            print("Running TaskCleanupDisk!", dt0, dt1)
+            self.clean(dt1)
+
+        if self.next_clean_dt <= dt1:
+            self.next_clean_dt = datetime(dt1.year, dt1.month, dt1.day, 3, 33, 33)
+            if self.next_clean_dt <= dt1:
+                self.next_clean_dt += timedelta(days=1)
+            print("Next disk cleanup is scheduled for", self.next_clean_dt)
 
         return 0
 
@@ -79,7 +151,7 @@ class CaptureHandler:
             except requests.exceptions.RequestException as e:
                 print(f"Requests raised an exception: {e}")
 
-    def take_action(self, now, importance):
+    def take_action(self, now, importance, camera):
         if (now - self.last_action_time).total_seconds() < 10.0:
             if importance <= self.last_action_impt:
                 print("Skipping action, throttling.")
@@ -88,7 +160,7 @@ class CaptureHandler:
         self.last_action_time = now
         self.last_action_impt = importance
 
-        filename = f'{self.pictures_dir}/pic_{now.strftime("%Y%m%d_%H%M%S")}_{importance}.jpg'
+        filename = f'{self.pictures_dir}/{get_pic_stem_from_data(now, importance)}.jpg'
 
         print(f"Capturing picture, saving to {filename} ...")
         camera.capture(filename)
@@ -99,62 +171,69 @@ class CaptureHandler:
 
 PICTURES_DIR = '/var/HallCam/pictures/'
 
-if not os.access(PICTURES_DIR, os.W_OK):
-    print(f"Cannot access {PICTURES_DIR}.")
-else:
-    print(f"Access to {PICTURES_DIR} is good.")
 
-print(f"localconfig.UPLOAD_URL = {localconfig.UPLOAD_URL}")
-print("")
+def run_camera():
+    if not os.access(PICTURES_DIR, os.W_OK):
+        print(f"Cannot access {PICTURES_DIR}.")
+    else:
+        print(f"Access to {PICTURES_DIR} is good.")
+
+    print(f"localconfig.UPLOAD_URL = {localconfig.UPLOAD_URL}")
+    print("")
 
 
-camera = PiCamera()
-camera.rotation = 180
+    camera = PiCamera()
+    camera.rotation = 180
 
-# The maximum resolution is 2592×1944 for still photos, 1920×1080 for video recording.
-# We also need to set the frame rate to 15 to enable this maximum resolution.
-# The minimum resolution is 64×64.
-CAMERA_MIN_RESOLUTION = (64, 64)
-CAMERA_LOW_RESOLUTION = (640, 480)
-CAMERA_MED_RESOLUTION = (1600, 900)
-CAMERA_MAX_RESOLUTION = (2592, 1944)
+    # The maximum resolution is 2592×1944 for still photos, 1920×1080 for video recording.
+    # We also need to set the frame rate to 15 to enable this maximum resolution.
+    # The minimum resolution is 64×64.
+    CAMERA_MIN_RESOLUTION = (64, 64)
+    CAMERA_LOW_RESOLUTION = (640, 480)
+    CAMERA_MED_RESOLUTION = (1600, 900)
+    CAMERA_MAX_RESOLUTION = (2592, 1944)
 
-camera.resolution = CAMERA_LOW_RESOLUTION
-camera.framerate = 15
+    camera.resolution = CAMERA_LOW_RESOLUTION
+    camera.framerate = 15
 
-# camera.annotate_background = Color('blue')
-# camera.annotate_foreground = Color('yellow')
-# camera.annotate_text = "Hallo!  :-)"
-# camera.annotate_text_size = 20  # 6 … 160, default is 32.
+    # camera.annotate_background = Color('blue')
+    # camera.annotate_foreground = Color('yellow')
+    # camera.annotate_text = "Hallo!  :-)"
+    # camera.annotate_text_size = 20  # 6 … 160, default is 32.
 
-cap_handler = CaptureHandler(PICTURES_DIR, localconfig.UPLOAD_URL)
-camera.start_preview()
-sleep(2)
+    cap_handler = CaptureHandler(PICTURES_DIR, localconfig.UPLOAD_URL)
+    camera.start_preview()
+    sleep(2)
 
-schedulers = [
-    SchedulerEvery5Minutes(),
-    SchedulerEveryFullHour(),
-  # SchedulerExposureMonitor(),
-]
+    tasks = [
+        TaskEvery5Minutes(),
+        TaskEveryFullHour(),
+      # TaskExposureMonitor(),
+        TaskCleanupDisk(PICTURES_DIR),
+    ]
 
-print("Entering main loop, press CTRL+C to exit.")
-try:
-    prev_dt = datetime.now()
+    print("Entering main loop, press CTRL+C to exit.")
+    try:
+        prev_dt = datetime.now()
 
-    while True:
-        curr_dt = datetime.now()
+        while True:
+            curr_dt = datetime.now()
 
-        importance = 0
-        for sch in schedulers:
-            importance = max(importance, sch.is_due(prev_dt, curr_dt))
+            importance = 0
+            for task in tasks:
+                importance = max(importance, task.is_due(prev_dt, curr_dt))
 
-        if importance > 0:
-            cap_handler.take_action(curr_dt, importance)
+            if importance > 0:
+                cap_handler.take_action(curr_dt, importance, camera)
 
-        prev_dt = curr_dt
-        sleep(1)
+            prev_dt = curr_dt
+            sleep(1)
 
-except KeyboardInterrupt:
-    print("\nExiting...")
+    except KeyboardInterrupt:
+        print("\nExiting ...")
 
-camera.stop_preview()
+    camera.stop_preview()
+
+
+if __name__ == "__main__":
+    run_camera()
