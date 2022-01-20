87eb6e7e465a339715ca8dc5722dd1051d1dfe1d
Carsten Fuchs <carsten.fuchs@cafu.de>
2022-01-20 10:18:34 +0100

    web: Show selected pictures of the user's cameras on the home page
    The details of the selection of the pictures and their presentation must
    still be improved, but this is at least a proper beginning.

--- web/Core/views/home.py
+++ web/Core/views/home.py
@@ -2,12 +2,35 @@ from datetime import date
 from django.db import transaction
 from django.shortcuts import render
 
+from Accounts.models import User
+from Core.models import Camera
+
 
 @transaction.non_atomic_requests
 def homepage_view(request):
     heute = date.today()
 
+    if request.user.is_authenticated:
+        cameras = request.user.camera_set.all()
+    else:
+        try:
+            public_user = User.objects.get(email="public_user@cafu.de", is_active=False)
+            cameras = public_user.camera_set.all()
+        except User.DoesNotExist:
+            cameras = Camera.objects.none()
+
+    cam_pics = []
+    for cam in cameras:
+        pics = reversed(cam.picture_set.all().order_by('-id')[:30])
+        cam_pics.append(
+            {
+                'camera': cam,
+                'pictures': pics,
+            },
+        )
+
     return render(request, "Core/home.html", {
-        'title': 'Hallo!',
+        'title': 'Übersicht',
         'heute': heute,
+        'cam_pics': cam_pics,
     })
