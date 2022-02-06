9ecf90b07ce2072efe0d4df1b685244dc00c26dc
Carsten Fuchs <carsten.fuchs@cafu.de>
2022-02-06 17:06:49 +0100

    web: Add a disk usage and cleanup report to the stats page

--- web/Core/libs/pictures.py
+++ web/Core/libs/pictures.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from pathlib import Path
+from shutil import disk_usage
 from django.conf import settings
 from Core.models import Picture
 
@@ -76,3 +78,131 @@ def sync_pictures(hot_run, verbosity=1):
                 thumb_path.delete()
 
     return stats
+
+
+def fmt_bytes(size, decimal_places=2):
+    for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']:
+        if size < 1024.0 or unit == 'PiB':
+            break
+        size /= 1024.0
+
+    return f"{size:.{decimal_places}f} {unit:3}"
+
+
+def get_data_from_pic_stem(pic_stem):
+    try:
+        dt = datetime.strptime(pic_stem[4:19], "%Y%m%d_%H%M%S")
+    except ValueError:
+        dt = datetime(2000, 1, 1)
+
+    try:
+        importance = int(pic_stem[20:21])
+    except ValueError:
+        importance = 0
+
+    # cross_check = get_pic_stem_from_data(dt, importance)
+    # if pic_stem != cross_check:
+    #     # print(f"Bad pic_stem: {pic_stem} != {cross_check}")
+    #     pass
+
+    return dt, importance
+
+
+def get_score(dt, importance, now):
+    """
+    This function computes the score that indicates the significance of a picture.
+    The significance determines the order in which pictures are deleted when disk
+    space gets scarce.
+
+    Some notes:
+
+      - Older pictures should be deleted before newer pictures.
+      - Less important pictures should be deleted before more important pictures.
+
+    For example, a picture that is unimportant but only a day old could have
+    the same significance (priority for deletion) as another picture that is
+    very important but now several years old.
+
+    That is, if we put the age and the importance of a picture into a coordinate
+    system, the picture with the coordinate p1 = (age: 1 day, importance: 1)
+    should have the same score as a picture at coordinate p2 = (age: 8 years,
+    importance: 8).
+
+    Note that we can interpolate between p1 and p2 and that the line through
+    p1 and p2 can be considered a 2-dimensional hyperplane. The distance of any
+    point to that hyperplane can be taken as the score of that point.
+    """
+    age = (now - dt).total_seconds() / 3600.0   # age in hours
+    vec = (age, importance)
+    nrm = (-(8 - 1), (365*8 - 1)*24.0)
+
+    return vec[0]*nrm[0] + vec[1]*nrm[1]
+
+
+def gather_pictures(clean_dir, now):
+    pics = []
+
+    for pic_path in Path(clean_dir).iterdir():
+        pics.append(
+            (
+                pic_path,
+                pic_path.stat().st_size,
+                get_score(*get_data_from_pic_stem(pic_path.stem), now),
+            )
+        )
+
+    # Sort the pictures by score.
+    pics.sort(key=lambda pic: -pic[2])
+    return pics
+
+
+def cleanup_pictures(hot_run, out=None):
+    GiB = pow(1024, 3)
+    MIN_PICS_TOTAL =  1*GiB
+    MAX_PICS_TOTAL = 20*GiB
+    MIN_DISK_FREE  =  2*GiB
+
+    disk_total, disk_used, disk_free = disk_usage("/")
+    btd_disk = max(MIN_DISK_FREE - disk_free, 0)
+
+    print(f"{fmt_bytes(disk_total):>10} disk space total", file=out)
+    print(f"{fmt_bytes(disk_used):>10} disk space used", file=out)
+    print(f"{fmt_bytes(disk_free):>10} disk space free", file=out)
+    print(f"{fmt_bytes(MIN_DISK_FREE):>10} disk space to be kept free (MIN_DISK_FREE)", file=out)
+    print(f"{fmt_bytes(btd_disk):>10} to delete (by disk space limits)", file=out)
+
+    pics_dir = Path(settings.MEDIA_ROOT, Picture.PICTURES_SUBDIR)
+    pics = gather_pictures(pics_dir, datetime.now())
+    pics_total_bytes = sum(pic[1] for pic in pics)
+    btd_pics = max(pics_total_bytes - MAX_PICS_TOTAL, 0)
+
+    print(f"\n{len(pics)} pictures", file=out)
+    print(f"{fmt_bytes(pics_total_bytes):>10} picture space taken", file=out)
+    print(f"{fmt_bytes(MAX_PICS_TOTAL):>10} picture space limit (MAX_PICS_TOTAL)", file=out)
+    print(f"{fmt_bytes(btd_pics):>10} to delete (by picture space limits)", file=out)
+
+    delete_at_most = max(pics_total_bytes - MIN_PICS_TOTAL, 0)
+    pic_bytes_to_delete = min(max(btd_disk, btd_pics), delete_at_most)
+
+    print(f"\ncombined", file=out)
+    print(f"{fmt_bytes(MIN_PICS_TOTAL):>10} minimum picture space to keep (MIN_PICS_TOTAL)", file=out)
+    print(f"{fmt_bytes(pic_bytes_to_delete):>10} to delete (by combined limits)", file=out)
+
+    while pics and pic_bytes_to_delete > 0:
+        pic = pics.pop()
+
+        if hot_run:
+            pic[0].unlink(missing_ok=True)
+            print(f"  deleted {pic[0]},{pic[1]:>8} bytes, score {pic[2]}", file=out)
+
+            num_del, details = Picture.objects.filter(filename=pic[0].name).delete()
+            if num_del != 1:
+                print("  WARNING: Could not delete the related `Picture` object!", file=out)
+        else:
+            print(f"  would delete {pic[0]},{pic[1]:>8} bytes, score {pic[2]}", file=out)
+
+        pic_bytes_to_delete -= pic[1]
+        pics_total_bytes -= pic[1]
+
+    print(f"\n{len(pics)} pictures left", file=out)
+    print(f"{fmt_bytes(pics_total_bytes):>10} picture space taken", file=out)
--- web/Core/views/stats.py
+++ web/Core/views/stats.py
@@ -1,15 +1,22 @@
+from io import StringIO
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
-from Core.libs.pictures import sync_pictures
+from Core.libs.pictures import cleanup_pictures, sync_pictures
 
 
 @staff_member_required
 def view(request):
+
+    with StringIO() as buffer:
+        cleanup_pictures(hot_run=False, out=buffer)
+        cleanup_report = buffer.getvalue()
+
     return render(
         request,
         'Core/stats.html',
         {
             'title': "Statistics 📈",
             'stats': sync_pictures(hot_run=False, verbosity=0),
+            'cleanup_report': cleanup_report,
         },
     )
