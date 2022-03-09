fbc07aca5ae1acd4d7497b9c13760f0394a42a07
Carsten Fuchs <carsten.fuchs@cafu.de>
2022-03-09 19:25:27 +0100

    web: Switch to GLightbox for all image galleries

--- web/HallCam/settings.py
+++ web/HallCam/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Absolute path to the directory the static files should be collected to.
 # Example: "/var/www/example.com/static/"
