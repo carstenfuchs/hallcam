9c225c4c0f1468fcd05a7bcc6b2acb020e146ace
Carsten Fuchs <carsten.fuchs@cafu.de>
2022-04-03 13:46:22 +0200

    web: Set proper default timezone
    If nothing else is set, times are rendered in templates in the default
    timezone.

--- web/HallCam/settings.py
+++ web/HallCam/settings.py
@@ -113,7 +113,7 @@ LOGIN_REDIRECT_URL = 'core:home'  # Ziel nach erfolgreichem Login, falls (mit "n
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_TZ = True
 
